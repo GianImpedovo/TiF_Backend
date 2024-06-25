0b2232383c1cd0f7f98e62f1402528342096a03d
Alejandro Biarrieta <103573136+Reinapepeada@users.noreply.github.com>
2024-06-25 12:46:41 -0300

    Update lista_materiales.js

--- src/routes/lista_materiales.js
+++ src/routes/lista_materiales.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/', listaController.agregarListado)
 router.get('/', listaController.obtenerListados)
 router.get('/proveedores', listaController.obtenerTodosProveedores)
-router.get('/recomendaciones', listaController.obtenerRecomendaciones)
+router.post('/recomendaciones', listaController.obtenerRecomendaciones)
 router.post('/guardarProveedores', listaController.agregarListadoAProveedor)
 
 module.exports = router;
